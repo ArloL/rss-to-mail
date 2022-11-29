e4f251c814e3fa343ae66f52caf005b2c7529957
Arlo O'Keeffe <tiger@k5d.de>
2022-11-29 11:47:00 +0100

    add merge to repositories

--- src/main/java/io/github/arlol/feed/ChannelRepository.java
+++ src/main/java/io/github/arlol/feed/ChannelRepository.java
@@ -3,9 +3,19 @@ package io.github.arlol.feed;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ChannelRepository extends CrudRepository<Channel, Long> {
 
 	Optional<Channel> findByLink(String link);
+
+	@Transactional
+	default Channel mergeByLink(Channel channel) {
+		return save(
+				findByLink(channel.getLink())
+						.map(c -> channel.toBuilder().id(c.getId()).build())
+						.orElse(channel)
+		);
+	}
 
 }
--- src/main/java/io/github/arlol/feed/FeedItemRepository.java
+++ src/main/java/io/github/arlol/feed/FeedItemRepository.java
@@ -3,9 +3,19 @@ package io.github.arlol.feed;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface FeedItemRepository extends CrudRepository<FeedItem, Long> {
 
 	Optional<FeedItem> findByGuid(String guid);
+
+	@Transactional
+	default FeedItem mergeByGuid(FeedItem feedItem) {
+		return save(
+				findByGuid(feedItem.getGuid())
+						.map(fi -> feedItem.toBuilder().id(fi.getId()).build())
+						.orElse(feedItem)
+		);
+	}
 
 }
