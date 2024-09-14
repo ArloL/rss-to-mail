fccb2316a239e7ce75b9761c47f633874c34e94f
Arlo O'Keeffe <tiger@k5d.de>
2024-09-14 16:01:20 +0200

    send If-None-Match and If-Modified-Since headers

--- src/main/java/io/github/arlol/RssToMailApplication.java
+++ src/main/java/io/github/arlol/RssToMailApplication.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.Optional;
 
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.ProtocolException;
 import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -22,9 +25,12 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import io.github.arlol.RssToMailProperties.Config;
 import io.github.arlol.feed.Channel;
 import io.github.arlol.feed.ChannelRepository;
+import io.github.arlol.feed.Feed;
+import io.github.arlol.feed.Feed.FeedBuilder;
 import io.github.arlol.feed.FeedItem;
 import io.github.arlol.feed.FeedItemProcessor;
 import io.github.arlol.feed.FeedItemRepository;
+import io.github.arlol.feed.FeedRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
@@ -49,6 +55,8 @@ public class RssToMailApplication implements ApplicationRunner {
 	@Autowired
 	FeedItemRepository feedItemRepository;
 	@Autowired
+	FeedRepository feedRepository;
+	@Autowired
 	ChannelRepository channelRepository;
 	@Autowired
 	FeedItemProcessor feedItemProcessor;
@@ -71,7 +79,37 @@ public class RssToMailApplication implements ApplicationRunner {
 
 	private void process(Config config, Channel channel) {
 		for (String url : channel.getFeeds()) {
-			executeSilentGet(url, response -> {
+
+			Feed feed = feedRepository
+					.findByChannelIdAndUrl(channel.getId(), url)
+					.orElseGet(() -> {
+						return feedRepository.save(
+								Feed.builder()
+										.channelId(channel.getId())
+										.url(url)
+										.build()
+						);
+					});
+
+			FeedBuilder feedBuilder = feed.toBuilder();
+
+			executeHttpRequest(feed, response -> {
+
+				silentGetHeader(response, "ETag").map(Header::getValue)
+						.ifPresent(etag -> {
+							feedBuilder.etag(etag);
+						});
+
+				silentGetHeader(response, "Last-Modified").map(Header::getValue)
+						.ifPresent(lastModified -> {
+							feedBuilder.lastModified(lastModified);
+						});
+
+				if (response.getCode() == 304) {
+					log.info("skipped since it was not modified: {}", feed);
+					return;
+				}
+
 				var articles = new RssReader()
 						.read(response.getEntity().getContent())
 						.map(item -> toFeedItem(item, channel))
@@ -94,14 +132,28 @@ public class RssToMailApplication implements ApplicationRunner {
 						.map(feedItemRepository::mergeByGuid)
 						.map(item -> item.getTitle())
 						.toList();
-				log.info("got these articles from feed {}: {}", url, articles);
+				log.info("got these articles from feed {}: {}", feed, articles);
 			});
 
+			feedRepository.save(feedBuilder.build());
 		}
+
 		if (rssToMailProperties.isMailSendingEnabled()) {
 			while (feedItemProcessor
 					.processMails(channel, config.getFrom(), config.getTo())) {
 			}
+		}
+
+	}
+
+	private Optional<Header> silentGetHeader(
+			ClassicHttpResponse response,
+			String name
+	) {
+		try {
+			return Optional.ofNullable(response.getHeader(name));
+		} catch (ProtocolException e) {
+			throw new IllegalStateException(e);
 		}
 	}
 
@@ -112,17 +164,26 @@ public class RssToMailApplication implements ApplicationRunner {
 
 	}
 
-	public <T> T executeSilentGet(
-			final String url,
+	public <T> T executeHttpRequest(
+			final Feed feed,
 			final ClassicHttpResponseConsumer consumer
 	) {
 		try {
-			var request = ClassicRequestBuilder.get(url).build();
-			return httpClient.execute(request, null, response -> {
-				consumer.accept(response);
-				return null;
-			});
+			var requestBuilder = ClassicRequestBuilder.get(feed.url());
+			if (feed.etag() != null) {
+				requestBuilder.addHeader("If-None-Match", feed.etag());
+			}
+			if (feed.lastModified() != null) {
+				requestBuilder
+						.addHeader("If-Modified-Since", feed.lastModified());
+			}
+			return httpClient
+					.execute(requestBuilder.build(), null, response -> {
+						consumer.accept(response);
+						return null;
+					});
 		} catch (IOException e) {
+			log.error("Could not get feed {}", feed, e);
 			throw new UncheckedIOException(e);
 		}
 	}
