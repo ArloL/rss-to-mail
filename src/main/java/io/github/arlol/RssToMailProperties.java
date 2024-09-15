702a6e6ab0ece7f1a208b4036c2d78f187095503
Arlo O'Keeffe <tiger@k5d.de>
2024-09-15 19:58:22 +0200

    add default values to RssToMailProperties

--- src/main/java/io/github/arlol/RssToMailProperties.java
+++ src/main/java/io/github/arlol/RssToMailProperties.java
@@ -2,6 +2,7 @@ package io.github.arlol;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -14,11 +15,20 @@ public record RssToMailProperties(
 		List<Config> configs
 ) {
 
+	public RssToMailProperties {
+		configs = Optional.ofNullable(configs).orElse(List.of());
+	}
+
 	public static record Config(
 			String from,
 			String[] to,
 			List<Channel> channels
 	) {
+
+		public Config {
+			to = Optional.ofNullable(to).orElse(new String[0]);
+			channels = Optional.ofNullable(channels).orElse(List.of());
+		}
 
 		@Override
 		public String toString() {
